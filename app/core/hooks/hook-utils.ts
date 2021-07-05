36f9467b05ac83fda5eafb9939605e884086ecbd
ttytango <59534146+ttytango@users.noreply.github.com>
2021-07-05 17:31:55 +0100

    created layout

---
+++ app/core/hooks/hook-utils.ts
@@ -0,0 +1,9 @@
+export const fetcher = (url, data = undefined) =>
+  fetch("http://localhost:3000" + url, {
+    method: data ? "POST" : "GET",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((res) => res.json())
---
+++ app/core/hooks/useFeed.ts
@@ -0,0 +1,16 @@
+// // import useSWR from "swr"
+// // import { fetcher } from "./api-utils"
+
+// function useFeed() {
+//   // const { data: feed } = useSWR("/api/forum/posts", fetcher)
+//   // return { feed }
+//   cosnt { feed } = useEffect(() => {
+//     fetch("/")
+//     return () => {
+//       cleanup
+//     };
+//   }, [input]);
+
+// }
+
+// export { useFeed }
