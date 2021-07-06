7e9632cf569eb9cadfa9a869d193177da0f8ce24
ttytango <59534146+ttytango@users.noreply.github.com>
2021-07-06 13:36:56 +0100

    removed unnecessary code

--- app/core/layouts/NavItem.js
+++ app/core/layouts/NavItem.js
@@ -1,48 +1,7 @@
 import { Link, Router, Routes, Ctx } from "blitz"
 import React, { useState } from "react"
 
-// class NavItem extends React.Component {
-//   constructor(props) {
-//     super(props);
-//       this.Ctx = this.props.Ctx
-//       this.children = this.props.children
-//       this.href = this.props.href;
-//       this.isActive =this.props.isActive;
-//
-//   }
-//
-//   render() {
-//     // window.location.href
-//
-//     return (
-//       <li>
-//         <Link href={this.href}>
-//           <a isActive={className}
-//             className={`block px-4 py-2 rounded-md ${this.isActive ? 'bg-blue-100 text-blue-700' : ''}`}
-//           {...this.props}>
-//             {this.children}
-//           </a>
-//         </Link>
-//       </li>
-//     )
-//   }
-//
-// }
-// export default NavItem
 export default function NavItem({ href, /*isActive, */ children, activeHandler }) {
-  // const [isActive, setIsActive] = useState(false)
-
-  // const windowPath = {window}.location.pathname === to;
-  // let className = isActive ? 'active' : '';
-
-  // const activeHandler = (event) => {
-  //   event.preventDefault();
-  //   if (isActive) {
-  //     setIsActive(false)
-  //   } else {
-  //     setIsActive(true)
-  //   }
-  // }
   return (
     <li>
       <Link href={href}>
